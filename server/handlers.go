2e4ee05d57378547538e657ab5c21d08f7c48c2e
Ivo Stoyanov <stoyanov@braincorporation.com>
2019-12-05 10:45:49 -0800

    net

--- server/handlers.go
+++ server/handlers.go
@@ -2,12 +2,11 @@ package server
 
 import (
 	"context"
-	"net"
-	"strings"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	v1 "github.com/ivost/shared/grpc/myvault"
+	"github.com/ivost/shared/pkg/system"
 	"github.com/ivost/shared/pkg/version"
 )
 
@@ -18,40 +17,8 @@ func (s *Server) Health(ctx context.Context, none *empty.Empty) (resp *v1.Health
 		Build:   version.Build,
 		Status:  "OK",
 		Time:    time.Now().Format(time.RFC3339),
-		Address: MyIP(),
+		Address: system.MyIP(),
 	}
-	return
-}
 
-func MyIP() string {
-	ifaces, err := net.Interfaces()
-	// handle err
-	_ = err
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-		// handle err
-		_ = err
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			if ip == nil {
-				continue
-			}
-			s := ip.String()
-			if strings.Contains(s, ":") {
-				continue
-			}
-			if s == "127.0.0.1" {
-				continue
-			}
-			//log.Printf("addr: %v", ip)
-			return s
-		}
-	}
-	return ""
+	return
 }
